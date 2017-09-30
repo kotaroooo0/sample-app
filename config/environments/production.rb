c6acbe1f7df3bc018e3968fdfacc65ac085c96f9
Adachi Kotaro <>
2017-09-30 21:16:54 +0900

    config.action_mailer.raise_delivery_errors => false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   host = 'shielded-everglades-20174.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
