c378194fd3e1d5071cac0151941a3f41b4d6c5d0
Adachi Kotaro <>
2017-10-04 21:14:00 +0900

    add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only:[:index,:edit, :update, :destroy]
+  before_action :logged_in_user, only:[:index,:edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only:[:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -21,9 +21,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        @user.send_activation_email
-        flash[:info] = "Please check your email to activate your account."
-        redirect_to root_url
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -52,6 +52,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
