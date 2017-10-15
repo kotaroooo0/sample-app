d26d3275a82dfe2d14b78feb2ab306fa7543a800
Adachi Kotaro <>
2017-10-16 03:11:24 +0900

    change micropost => @micropost to give ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, micropost_id: params[:micropost_id])
     @likes = Like.where(micropost_id: params[:micropost_id])
+    @micropost = Micropost.find(params[:micropost_id])
     respond_to do |format|
       format.html
       format.js
@@ -12,6 +13,7 @@ class LikesController < ApplicationController
     like = Like.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
     like.destroy
     @likes = Like.where(micropost_id: params[:micropost_id])
+    @micropost = Micropost.find(params[:micropost_id])
     respond_to do |format|
       format.html
       format.js
