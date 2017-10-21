6ec53e347702376f3bc389d2a42806013be6b8a0
Adachi Kotaro <>
2017-10-21 16:12:53 +0900

    fix rest-api but don't work...

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -1,6 +1,22 @@
 module Api
   module V1
     class MicropostsController < ApplicationController
+      def create
+        @micropost = User.find(params[:user_id]).microposts.build(micropost_params)
+        @micropost.saveReplyTarget
+        @micropost.save
+      end
+
+      def destroy
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        @micropost.destroy
+      end
+
+      private
+
+      def micropost_params
+        params.require(:micropost).permit(:content, :picture)
+      end
     end
   end
 end
